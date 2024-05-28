b2f78db5b7a190e1a7effa7716e219772629d545
data-miner00 <data-miner00@users.noreply.github.com>
2024-05-28 12:57:24 +0800

    chore: Add integration project

---
+++ src/Sway.Integrations/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Sway.Integrations
+{
+    using System.Runtime.CompilerServices;
+
+    public class Class1 : IAsyncStateMachine
+    {
+        public void MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
