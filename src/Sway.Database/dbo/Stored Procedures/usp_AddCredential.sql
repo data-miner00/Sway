e5aeeb4c346fcdc93ceb36dfd600a436b46fca49
data-miner00 <data-miner00@users.noreply.github.com>
2024-05-29 21:50:21 +0800

    feat: Add insert credential procedure

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_AddCredential.sql
@@ -0,0 +1,35 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE usp_AddCredential
+	@PasswordHash NVARCHAR(100),
+	@PasswordSalt NVARCHAR(50),
+	@HashAlgorithm NVARCHAR(50),
+	@RowId UNIQUEIDENTIFIER OUTPUT
+AS
+BEGIN
+	DECLARE @OutputTable TABLE (Id UNIQUEIDENTIFIER);
+	SET NOCOUNT, XACT_ABORT ON;
+
+    BEGIN TRANSACTION;
+		
+		INSERT INTO [dbo].[Credentials]
+		(
+			[PasswordHash],
+			[PasswordSalt],
+			[HashAlgorithm]
+		)
+		OUTPUT inserted.Id INTO @OutputTable
+		VALUES
+		(
+			@PasswordHash,
+			@PasswordSalt,
+			@HashAlgorithm
+		);
+
+	COMMIT TRANSACTION;
+
+	SELECT @RowId = Id FROM @OutputTable;
+END
