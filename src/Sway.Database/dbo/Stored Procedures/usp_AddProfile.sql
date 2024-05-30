64dc4ff220a82426722bb7804914ccc0a88fba08
data-miner00 <data-miner00@users.noreply.github.com>
2024-05-30 21:35:55 +0800

    feat: Add functionality

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_AddProfile.sql
@@ -0,0 +1,36 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE usp_AddProfile
+	@Email NVARCHAR(50),
+	@Phone NVARCHAR(50),
+	@PhotoUrl NVARCHAR(255),
+	@Description NVARCHAR(255),
+	@RowId UNIQUEIDENTIFIER OUTPUT
+AS
+BEGIN
+	DECLARE @OutputTable TABLE (Id UNIQUEIDENTIFIER);
+	SET NOCOUNT, XACT_ABORT ON;
+
+    BEGIN TRANSACTION;
+		
+		INSERT INTO [dbo].[UserProfiles]
+		(
+			[Email],
+			[Phone],
+			[PhotoUrl],
+			[Description]
+		)
+		VALUES
+		(
+			@Email,
+			@Phone,
+			@PhotoUrl,
+			@Description
+		);
+
+	COMMIT TRANSACTION;
+	SELECT @RowId = Id FROM @OutputTable;
+END
---
+++ src/Sway.Database/dbo/Stored Procedures/usp_CreateNewUser.sql
@@ -0,0 +1,49 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE usp_CreateNewUser
+	@Username NVARCHAR(50),
+	@DateOfBirth DATETIME2(7),
+	@Email NVARCHAR(50),
+	@Phone NVARCHAR(50),
+	@PhotoUrl NVARCHAR(255),
+	@Description NVARCHAR(255),
+	@PasswordHash NVARCHAR(100),
+	@PasswordSalt NVARCHAR(50),
+	@HashAlgorithm NVARCHAR(50)
+AS
+BEGIN
+	DECLARE @CredentialId UNIQUEIDENTIFIER;
+	DECLARE @ProfileId UNIQUEIDENTIFIER;
+
+	SET NOCOUNT, XACT_ABORT ON;
+
+    BEGIN TRANSACTION;
+		
+		EXEC [dbo].[usp_AddCredential] @PasswordHash, @PasswordSalt, @HashAlgorithm, @CredentialId OUTPUT;
+
+		EXEC [dbo].[usp_AddProfile] @Email, @Phone, @PhotoUrl, @Description, @ProfileId OUTPUT;
+
+		INSERT INTO [dbo].[Users]
+		(
+			[Username],
+			[Status],
+			[ProfileId],
+			[CredentialId],
+			[Role],
+			[DateOfBirth]
+		)
+		VALUES
+		(
+			@Username,
+			'Active',
+			@ProfileId,
+			@CredentialId,
+			'User',
+			@DateOfBirth
+		);
+
+	COMMIT TRANSACTION;
+END
