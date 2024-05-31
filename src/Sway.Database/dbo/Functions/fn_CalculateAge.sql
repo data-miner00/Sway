bafb36cbcd2c1c1e5e9a3f6e7055fd98facac3e4
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-01 00:28:37 +0800

    feat: Add calculate age function

---
+++ src/Sway.Database/dbo/Functions/fn_CalculateAge.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[fn_CalculateAge]
+(
+	@DateOfBirth DATE
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @Age INT;
+	SET @Age
+		= DATEDIFF(YEAR, @DateOfBirth, GETDATE())
+		  - CASE
+				WHEN DATEADD(YEAR, DATEDIFF(YEAR, @DateOfBirth, GETDATE()), @DateOfBirth) > GETDATE() THEN
+					1
+				ELSE
+					0
+			END;
+
+	RETURN @Age;
+END
