328e4f37d6cf11b9ef8db6f9ceb66ddf93aec394
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-02 09:59:38 +0800

    feat: Add address stored procs

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_DeleteAddressById.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE usp_DeleteAddressById
+	@Id UNIQUEIDENTIFIER
+AS
+BEGIN
+	SET NOCOUNT, XACT_ABORT ON;
+
+	BEGIN TRANSACTION;
+
+	DELETE FROM [dbo].[Addresses]
+	WHERE [Id] = @Id;
+
+	COMMIT TRANSACTION;
+
+	RETURN @@ERROR;
+END
---
+++ src/Sway.Database/dbo/Stored Procedures/usp_UpdateAddress.sql
@@ -0,0 +1,35 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE usp_UpdateAddress
+	@Id UNIQUEIDENTIFIER,
+	@Type NVARCHAR(50),
+	@Street1 NVARCHAR(255),
+	@Street2 NVARCHAR(255),
+	@City NVARCHAR(50),
+	@State NVARCHAR(50),
+	@Postcode NVARCHAR(50),
+	@Country NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT, XACT_ABORT ON;
+
+	BEGIN TRANSACTION;
+
+	UPDATE [dbo].[Addresses]
+	SET
+		[Type] = @Type,
+		[Street1] = @Street1,
+		[Street2] = @Street2,
+		[City] = @City,
+		[State] = @State,
+		[Postcode] = @Postcode,
+		[Country] = @Country
+	WHERE [Id] = @Id;
+
+	COMMIT TRANSACTION;
+
+	RETURN @@ERROR;
+END
