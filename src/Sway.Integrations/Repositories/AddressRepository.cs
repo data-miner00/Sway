2ebb1ba7b90f36b5c7f59db7b149c39d01ce940d
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-02 09:40:48 +0800

    feat: Add address repository

---
+++ src/Sway.Integrations/Repositories/AddressRepository.cs
@@ -0,0 +1,87 @@
+﻿namespace Sway.Integrations.Repositories;
+
+using Dapper;
+using Sway.Core.Models;
+using Sway.Core.Repositories;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The repository for <see cref="Address"/>.
+/// </summary>
+public sealed class AddressRepository : IAddressRepository
+{
+    private readonly IDbConnection connection;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AddressRepository"/> class.
+    /// </summary>
+    /// <param name="connection">The database connection.</param>
+    public AddressRepository(IDbConnection connection)
+    {
+        this.connection = connection;
+    }
+
+    /// <inheritdoc/>
+    public Task CreateAsync(Address address, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var command = new CommandDefinition(
+            "EXEC usp_AddAddress @Type, @Street1, @Street2, @City, @State, @Postcode, @Country;",
+            address,
+            commandType: CommandType.StoredProcedure);
+
+        return this.connection.ExecuteAsync(command);
+    }
+
+    /// <inheritdoc/>
+    public Task DeleteByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var command = new CommandDefinition(
+            "EXEC usp_DeleteAddressById @Id",
+            new { Id = id },
+            commandType: CommandType.StoredProcedure);
+
+        return this.connection.ExecuteAsync(command);
+    }
+
+    /// <inheritdoc/>
+    public Task<IEnumerable<Address>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var command = new CommandDefinition(
+            "SELECT * FROM Addresses;");
+
+        return this.connection.QueryAsync<Address>(command);
+    }
+
+    /// <inheritdoc/>
+    public Task<Address> GetByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var command = new CommandDefinition(
+            "SELECT * FROM Addresses WHERE Id = @Id;",
+            new { Id = id });
+
+        return this.connection.QueryFirstAsync<Address>(command);
+    }
+
+    /// <inheritdoc/>
+    public Task UpdateAsync(Address address, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var command = new CommandDefinition(
+            "EXEC usp_UpdateAddress @Id, @Type, @Street1, @Street2, @City, @State, @Postcode, @Country;",
+            address);
+
+        return this.connection.ExecuteAsync(command);
+    }
+}
