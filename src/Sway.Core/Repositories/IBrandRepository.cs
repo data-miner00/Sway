8e3db8b1a4b42c2ca2595ba796586266a8b734c4
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-05 21:38:40 +0800

    feat: Add brand repo and sp

---
+++ src/Sway.Core/Repositories/IBrandRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace Sway.Core.Repositories;
+
+using Sway.Core.Models;
+
+/// <summary>
+/// The contract for <see cref="Brand"/> repository.
+/// </summary>
+public interface IBrandRepository
+{
+    /// <summary>
+    /// Retrieves all brand.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The list of brands.</returns>
+    Task<IEnumerable<Brand>> GetAllAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Get brand by Id.
+    /// </summary>
+    /// <param name="id">The ID.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The found brand.</returns>
+    Task<Brand> GetByIdAsync(string id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Create a new brand entry.
+    /// </summary>
+    /// <param name="brand">The brand to be created.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The task.</returns>
+    Task CreateAsync(Brand brand, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates an existing brand.
+    /// </summary>
+    /// <param name="brand">The brand to be updated.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The task.</returns>
+    Task UpdateAsync(Brand brand, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Deletes a brand by Id.
+    /// </summary>
+    /// <param name="id">The brand ID.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The task.</returns>
+    Task DeleteByIdAsync(string id, CancellationToken cancellationToken);
+}
---
+++ src/Sway.Database/dbo/Stored Procedures/usp_AddBrand.sql
@@ -0,0 +1,30 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_AddBrand]
+	@Name NVARCHAR(50),
+	@Description NVARCHAR(255),
+	@LogoUrl NVARCHAR(255)
+AS
+BEGIN
+	SET NOCOUNT, XACT_ABORT ON;
+
+    BEGIN TRANSACTION;
+
+	INSERT INTO [dbo].[Brands]
+	(
+		[Name],
+		[Description],
+		[LogoUrl]
+	)
+	VALUES
+	(
+		@Name,
+		@Description,
+		@LogoUrl
+	);
+
+	COMMIT TRANSACTION;
+END
