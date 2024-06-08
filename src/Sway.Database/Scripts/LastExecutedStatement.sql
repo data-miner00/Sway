365efa179aaea578c231f5b9f013f6092844cb12
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-09 01:16:08 +0800

    feat: Add some scripts

---
+++ src/Sway.Database/Scripts/LastExecutedStatement.sql
@@ -0,0 +1,8 @@
+﻿SELECT txt.text,
+       qp.query_plan,
+       req.cpu_time,
+       req.logical_reads,
+       req.writes
+FROM sys.dm_exec_requests req
+    CROSS APPLY sys.dm_exec_query_plan(req.plan_handle) qp
+    CROSS APPLY sys.dm_exec_sql_text(req.plan_handle) txt;
---
+++ src/Sway.Database/Scripts/PreviousExecutedQueries.sql
@@ -0,0 +1,14 @@
+﻿-- Previously executed queries
+SELECT TOP 5
+    txt.[text],
+    qp.query_plan,
+    st.execution_count,
+    st.min_logical_writes,
+    st.max_logical_reads,
+    st.total_logical_reads,
+    st.total_elapsed_time,
+    st.last_elapsed_time
+FROM sys.dm_exec_query_stats st
+    CROSS APPLY sys.dm_exec_query_plan(st.plan_handle) qp
+    CROSS APPLY sys.dm_exec_sql_text(st.sql_handle) txt
+ORDER BY (st.total_elapsed_time) DESC;
