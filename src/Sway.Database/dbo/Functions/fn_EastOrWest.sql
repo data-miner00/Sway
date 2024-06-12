6c2c409420ccea0f51266f625c0f28f732210cf9
data-miner00 <data-miner00@users.noreply.github.com>
2024-06-12 22:07:53 +0800

    feat: Add more scripts

---
+++ src/Sway.Database/dbo/Functions/fn_EastOrWest.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[fn_EastOrWest]
+(
+	@longitude DECIMAL(9, 6)
+)
+RETURNS CHAR(4)
+AS
+BEGIN
+	DECLARE @result CHAR(4) = 'same';
+	IF (@longitude > 0.00) SET @result = 'east';
+	IF (@longitude < 0.00) SET @result = 'west';
+
+	RETURN @result;
+END
---
+++ src/Sway.Database/dbo/Stored Procedures/usp_SearchAddress.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_SearchAddress]
+	@Keyword NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT TOP 5 * FROM [dbo].[Addresses]
+	WHERE [Street1] LIKE '%' + @Keyword + '%'
+	OR [Street2] LIKE '%' + @Keyword + '%'
+	OR [City] LIKE '%' + @Keyword + '%'
+	OR [State] LIKE '%' + @Keyword + '%'
+	OR [Postcode] LIKE '%' + @Keyword + '%'
+	OR [Country] LIKE '%' + @Keyword + '%';
+
+	RETURN @@ERROR;
+END
