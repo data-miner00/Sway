a448b9a17e7f7df9f3ea4c99087e2a656ddc91ae
data-miner00 <data-miner00@users.noreply.github.com>
2024-07-08 21:19:12 +0800

    feat: Added order dedicated tables

---
+++ src/Sway.Database/dbo/Tables/OrderAddresses.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[OrderAddresses] (
+    [Id]         UNIQUEIDENTIFIER CONSTRAINT [DF_OrderAddresses_Id] NOT NULL DEFAULT NEWSEQUENTIALID() ROWGUIDCOL,
+    [Type]       NVARCHAR (50)  NOT NULL,
+    [Street1]    NVARCHAR (255) NOT NULL,
+    [Street2]    NVARCHAR (255) NULL,
+    [City]       NVARCHAR (50)  NOT NULL,
+    [State]      NVARCHAR (50)  NOT NULL,
+    [Postcode]   NVARCHAR (50)  NOT NULL,
+    [Country]    NVARCHAR (50)  NOT NULL,
+    [CreatedAt]  DATETIME2 (7)  NOT NULL DEFAULT GETDATE(),
+    [ModifiedAt] DATETIME2 (7)  NOT NULL DEFAULT GETDATE(),
+    CONSTRAINT [PK_OrderAddresses] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_OrderAddresses_OnUpdate]
+    ON [dbo].[OrderAddresses]
+    AFTER UPDATE
+    AS
+    BEGIN
+        SET NOCOUNT ON;
+
+        UPDATE [dbo].[OrderAddresses]
+        SET [ModifiedAt] = GETDATE()
+        FROM [dbo].[OrderAddresses] T
+        INNER JOIN inserted I ON T.Id = I.Id;
+    END;
---
+++ src/Sway.Database/dbo/Tables/OrderPaymentMethods.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[OrderPaymentMethods] (
+    [Id]         UNIQUEIDENTIFIER CONSTRAINT [DF_OrderPaymentMethods_Id] DEFAULT NEWID() NOT NULL,
+    [UserId]     UNIQUEIDENTIFIER NOT NULL,
+    [Type]       NVARCHAR (50) NOT NULL,
+    [Provider]   NVARCHAR (50) NOT NULL,
+    [AccountNo]  NVARCHAR (50) NOT NULL,
+    [ExpiryDate] DATETIME2 (7) NOT NULL,
+    [CreatedAt]  DATETIME2 (7) DEFAULT GETDATE() NOT NULL,
+    [ModifiedAt] DATETIME2 (7) DEFAULT GETDATE() NOT NULL,
+    CONSTRAINT [PK_OrderPaymentMethods] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_OrderPaymentMethods_Users] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id])
+);
+
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_OrderPaymentMethods_OnUpdate]
+	ON [dbo].[OrderPaymentMethods]
+	AFTER UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+
+		UPDATE [dbo].[OrderPaymentMethods]
+		SET [ModifiedAt] = GETDATE()
+		FROM [dbo].[OrderPaymentMethods] T
+		INNER JOIN inserted I ON T.ID = I.ID;    
+	END;
+
+
