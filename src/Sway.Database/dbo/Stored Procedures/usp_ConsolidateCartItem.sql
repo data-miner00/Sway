159a18fc7b63da0212bb5d9fc20b2e3d57208257
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-04 22:23:36 +0800

    feat: Add consolidate cart item procedure

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_ConsolidateCartItem.sql
@@ -0,0 +1,43 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_ConsolidateCartItem]
+AS
+BEGIN
+	DECLARE @Temp TABLE (
+		[ShoppingCartId] UNIQUEIDENTIFIER,
+		[ProductId] UNIQUEIDENTIFIER,
+		[Quantity] INT
+	);
+
+	SET NOCOUNT, XACT_ABORT ON;
+
+	BEGIN TRANSACTION
+
+	INSERT INTO @Temp
+    SELECT
+		[ShoppingCartId],
+		[ProductId],
+		SUM([Quantity]) AS Quantity
+	FROM [dbo].[CartItems]
+	GROUP BY [ShoppingCartId], [ProductId];
+
+	MERGE [dbo].[CartItems] T
+	USING @Temp S
+	ON (T.ShoppingCartId = S.ShoppingCartId AND T.ProductId = S.ProductId)
+	WHEN MATCHED THEN DELETE;
+
+	INSERT INTO [dbo].[CartItems]
+	(
+		[ShoppingCartId],
+		[ProductId],
+		[Quantity]
+	)
+	SELECT * FROM @Temp;
+
+	COMMIT TRANSACTION
+
+	RETURN @@ERROR;
+END
