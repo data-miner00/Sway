5eb72ad440de74a22a6ba233e5b5b017cf5b83f0
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-05 22:37:01 +0800

    feat: Add paginated product query procedure

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_GetAllProductsWithPagination.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_GetAllProductsWithPagination]
+	@OffsetCount INT = 0,
+	@FetchCount INT = 10
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT
+		[Id],
+		[Name],
+		[Description],
+		[Price],
+		[InStock],
+		[SKU],
+		[BrandId],
+		[CategoryId],
+		[CreatedAt],
+		[ModifiedAt],
+		[AverageRatings]
+	FROM
+		[dbo].[Products]
+	ORDER BY
+		[Price],
+		[Name]
+	OFFSET @OffsetCount ROWS 
+	FETCH NEXT @FetchCount ROWS ONLY;
+END
