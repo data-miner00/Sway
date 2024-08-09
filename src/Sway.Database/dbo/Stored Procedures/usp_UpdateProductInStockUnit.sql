c022c8e6733aa979ace94f60878e5ac5c82a5654
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-09 21:16:52 +0800

    feat: Add update stock unit procedure

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_UpdateProductInStockUnit.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_UpdateProductInStockUnit]
+	@ProductId UNIQUEIDENTIFIER,
+	@ByAmount INT,
+
+	-- If true, increment by the amount. Otherwise, decrement.
+	@IsIncrement BIT
+AS
+BEGIN
+	SET NOCOUNT, XACT_ABORT ON;
+
+    BEGIN TRANSACTION
+
+	If @IsIncrement = 1
+		UPDATE [dbo].[Products]
+		SET [InStock] = [InStock] + @ByAmount;
+	ELSE 
+		UPDATE [dbo].[Products]
+		SET [InStock] = [InStock] - @ByAmount;
+
+	COMMIT TRANSACTION
+
+	RETURN @@ERROR;
+END
