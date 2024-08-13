e06fdebcb8cdbc82952c7a432d5fb4f5da03a75f
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-13 22:04:35 +0800

    feat: Update first product name procedure

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_SetFirstProductName.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_SetFirstProductName]
+	@Name NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT, XACT_ABORT ON;
+
+    WITH vw_TopOneProduct AS (
+		SELECT TOP 1 * FROM
+		[dbo].[Products]
+	)
+	UPDATE vw_TopOneProduct
+	SET [Name] = @Name;
+
+END
