d980602b3654053b045cd3a451cc361b325f9e0e
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-18 18:50:13 +0800

    feat: Add checkout

---
+++ src/Sway.Web.Mvc/Controllers/CheckoutController.cs
@@ -0,0 +1,29 @@
+﻿namespace Sway.Web.Mvc.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Sway.Core.Repositories;
+
+public class CheckoutController : Controller
+{
+    private readonly IShoppingCartRepository repository;
+
+    public CheckoutController(IShoppingCartRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public CancellationToken CancellationToken => this.HttpContext.RequestAborted;
+
+    public async Task<IActionResult> Index()
+    {
+        var items = await this.repository.GetCartItemsByUserIdAsync(Constants.TestUserId, this.CancellationToken);
+
+        return this.View(items);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PlaceOrder()
+    {
+        return this.View();
+    }
+}
