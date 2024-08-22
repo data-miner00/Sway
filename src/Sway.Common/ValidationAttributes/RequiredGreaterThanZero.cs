591f03f2a9a1a6f6f5a5a3f1979cd2fb2d4d6870
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-22 21:39:41 +0800

    feat: Add greater than zero validation attribute

---
+++ src/Sway.Common/ValidationAttributes/RequiredGreaterThanZero.cs
@@ -0,0 +1,17 @@
+﻿namespace Sway.Common.ValidationAttributes;
+
+using System.ComponentModel.DataAnnotations;
+
+public sealed class RequiredGreaterThanZero : ValidationAttribute
+{
+    /// <summary>
+    /// Validates a value that is integer greater than zero.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <returns><c>true</c> if it is greater than zero.</returns>
+    public override bool IsValid(object? value)
+    {
+        return value != null && int.TryParse(value.ToString(), out var i) && i > 0;
+        return value is int integer && integer > 0;
+    }
+}
