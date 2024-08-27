3d1e015a3314552439248863614b05f8f5ce931a
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-27 23:25:15 +0800

    feat: Add get recent product procedure

---
+++ src/Sway.Database/dbo/Stored Procedures/usp_GetRecentlyAddedProductsByDays.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_GetRecentlyAddedProductsByDays]
+	@DaysFromToday INT = 30
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT * FROM [dbo].[Products]
+	WHERE DATEDIFF(DAY, [CreatedAt], GETDATE()) BETWEEN 0 AND @DaysFromToday;
+END
