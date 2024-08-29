cab22b4d31238dd3c6c76f5426f84a16e93243c4
data-miner00 <data-miner00@users.noreply.github.com>
2024-08-29 22:20:26 +0800

    feat: Add money extension for decimal

---
+++ src/Sway.Common/DecimalExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Sway.Common;
+
+public static class DecimalExtensions
+{
+    public static string ToMoney(this decimal value)
+    {
+        return value.ToString("0.00");
+    }
+
+    public static string ToMoney(this decimal value, string currencySymbol)
+    {
+        return string.Concat(currencySymbol, value.ToMoney());
+    }
+}
