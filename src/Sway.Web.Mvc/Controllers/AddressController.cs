fa774cfdca3cc59223227760b0f4da8fca1dc466
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-03 22:08:38 +0800

    feat: Base profile layout

---
+++ src/Sway.Web.Mvc/Controllers/AddressController.cs
@@ -0,0 +1,23 @@
+﻿namespace Sway.Web.Mvc.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Sway.Core.Repositories;
+
+[Route("Profile/[controller]/[action]")]
+public class AddressController : Controller
+{
+    private readonly IAddressRepository repository;
+
+    private CancellationToken CancellationToken => this.HttpContext.RequestAborted;
+
+    public AddressController(IAddressRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var addresses = await this.repository.GetAllByUserAsync(Constants.TestUserId, this.CancellationToken);
+        return this.View(addresses);
+    }
+}
