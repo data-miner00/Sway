15b4a68f3ffd4c2fc93e3968963b9d239002ee75
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-10 21:26:49 +0800

    feat: Add payment method index page

---
+++ src/Sway.Web.Mvc/Controllers/PaymentMethodController.cs
@@ -0,0 +1,24 @@
+﻿namespace Sway.Web.Mvc.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Sway.Core.Repositories;
+
+[Route("Profile/[controller]/[action]")]
+public class PaymentMethodController : Controller
+{
+    private readonly IPaymentMethodRepository repository;
+
+    private CancellationToken CancellationToken => this.HttpContext.RequestAborted;
+
+    public PaymentMethodController(IPaymentMethodRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var paymentMethods = await this.repository.GetAllByUserAsync(Constants.TestUserId, this.CancellationToken);
+
+        return this.View(paymentMethods);
+    }
+}
