9401fab66521ec5337981a4b80ddf52436da0bc1
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-21 18:00:47 +0800

    feat: Working update and delete method

--- src/Sway.Web.Mvc/Controllers/PaymentMethodController.cs
+++ src/Sway.Web.Mvc/Controllers/PaymentMethodController.cs
@@ -1,6 +1,8 @@
 ﻿namespace Sway.Web.Mvc.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Sway.Core.Dtos;
+using Sway.Core.Models;
 using Sway.Core.Repositories;
 
 [Route("Profile/[controller]/[action]")]
@@ -20,5 +22,44 @@ public class PaymentMethodController : Controller
         var paymentMethods = await this.repository.GetAllByUserAsync(Constants.TestUserId, this.CancellationToken);
 
         return this.View(paymentMethods);
+    }
+
+    [Route("{id:guid}")]
+    public async Task<IActionResult> Index(Guid id)
+    {
+        var paymentMethod = await this.repository.GetByIdAsync(id.ToString(), this.CancellationToken);
+
+        return this.Ok(paymentMethod);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update([FromBody] UpdatePaymentMethodRequest request)
+    {
+        var entity = new PaymentMethod
+        {
+            Id = request.Id,
+            Type = request.PaymentType,
+            Provider = request.Provider,
+            CVV = request.CVV,
+            ExpiryDate = request.ExpiryDate,
+            CardholderName = request.CardholderName,
+            CardIssuingBank = request.CardIssuingBank,
+            CardNumber = request.CardNumber,
+            CardIssuingCountry = request.CardIssuingCountry,
+            Currency = request.Currency,
+            Balance = request.Balance,
+        };
+
+        await this.repository.UpdateAsync(entity, this.CancellationToken);
+
+        return this.Ok();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await this.repository.DeleteByIdAsync(id.ToString(), this.CancellationToken);
+
+        return this.Ok();
     }
 }
