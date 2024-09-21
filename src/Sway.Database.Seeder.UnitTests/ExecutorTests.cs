2aefb20ccda2e8fa7821845fe914cb3831ece9fd
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-21 20:12:02 +0800

    test: Add unit test for seeder

---
+++ src/Sway.Database.Seeder.UnitTests/ExecutorTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Sway.Database.Seeder.UnitTests;
+
+using Moq;
+using Sway.Database.Seeder.Options;
+using Sway.Database.Seeder.Sinks;
+
+public sealed class ExecutorTests
+{
+    private static Mock<ISink> MockSink;
+    private static Mock<ISinkFactory> MockSinkFactory;
+    private static IExecutor Executor;
+    private static SeedingOption SeedingOption;
+
+    [Before(Test)]
+    public void Setup()
+    {
+        MockSink = new Mock<ISink>();
+        MockSinkFactory = new Mock<ISinkFactory>();
+
+        MockSinkFactory
+            .Setup(x => x.CreateSink(It.IsAny<SwayEntity>(), It.IsAny<SinkType>()))
+            .Returns(MockSink.Object);
+
+        SeedingOption = new SeedingOption
+        {
+            Count = 1,
+            Entity = SwayEntity.User,
+            Destination = SinkType.Database,
+        };
+        Executor = new Executor(MockSinkFactory.Object, SeedingOption);
+    }
+
+    [Test]
+    [Retry(3)]
+    public async Task ExecuteAsync_InvokedProvisionAsync()
+    {
+        await Executor.ExecuteAsync(default);
+
+        MockSink.Verify(
+            x => x.ProvisionAsync(1, default),
+            Times.Once());
+    }
+
+    [Test]
+    [DependsOn(nameof(ExecuteAsync_InvokedProvisionAsync))]
+    public async Task AfterExecution_ObjectNotNull()
+    {
+        await Assert.That(Executor).IsNotNull();
+    }
+}
---
+++ src/Sway.Database.Seeder/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("90f0fb08-0432-46c1-b99a-d12110b71fe3")]
+[assembly: InternalsVisibleTo("Sway.Database.Seeder.UnitTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
