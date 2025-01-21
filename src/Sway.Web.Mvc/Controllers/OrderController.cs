ba106d040fe0c7875a9dc5986ca058879a099fe0
Shaun Chong <data-miner00@users.noreply.github.com>
2025-01-21 22:39:11 +0800

    feat: Add basic order details

---
+++ src/Sway.Web.Mvc/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿namespace Sway.Web.Mvc.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Sway.Core.Repositories;
+
+public sealed class OrderController : Controller
+{
+    private readonly IOrderRepository repository;
+
+    public OrderController(IOrderRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public CancellationToken CancellationToken => this.HttpContext.RequestAborted;
+
+    public async Task<IActionResult> Index()
+    {
+        var orders = await this.repository.GetAllByUserIdAsync(Constants.TestUserId, this.CancellationToken);
+
+        return this.View(orders);
+    }
+
+    [Route("{id:guid}")]
+    public async Task<IActionResult> Details(Guid id)
+    {
+        var order = await this.repository.GetByIdAsync(id.ToString(), this.CancellationToken);
+
+        return this.View(order);
+    }
+}
